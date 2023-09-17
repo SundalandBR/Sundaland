edabeeadd03af1e5d0bd3cf6848cabc198317284
Sundaland <b449024525@outlook.com>
2023-09-17 10:50:48 +0800

    '实现Hash加权、合并、降维'

--- 202121331073/main.py
+++ 202121331073/main.py
@@ -25,7 +25,19 @@ def MD5Hash(string):
     md5.update(string.encode("utf-8"))
     return bin(int(md5.hexdigest()[8:-8],16))[2:].zfill(64) # 转化为64bit 2进制
 
-# Hash加权与合并
+# Hash加权、合并、降维
+def simhash(packetlist):
+    whash = [0] * 64 # 64bit
+    # 将求得的64bithash值加权求和
+    for hash,(word,weights) in packetlist:
+        for i,n in enumerate(hash):
+            t = weights if n == '1' else (-weights)
+            whash[i] += t
+    # 降维
+    for i,val in enumerate(whash):
+        whash[i] = 1 if val>0 else 0
+    return whash
+    
 
 org_hashlist = []
 org_add_hashlist = []
@@ -35,11 +47,8 @@ for word,weight in org_tag:
     org_hashlist.append(MD5Hash(word))
 for word,weight in orgadd_tag:
     org_add_hashlist.append(MD5Hash(word))
-
-print(od)
-print(oad)
-print(org_hashlist)
-print(org_add_hashlist)
+packlist = list(zip(org_add_hashlist,orgadd_tag))
+print(simhash(packlist))
 
 #精确模式
 
